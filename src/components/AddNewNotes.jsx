bee76f69e0fbf6cc95183e3e7929d74076427128
erfanmoulaee <erfanmoulaee1375@gmail.com>
2024-10-20 21:48:06 +0330

    s03-e04

---
+++ src/components/AddNewNotes.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "../App.css";
+
+function AddNewNotes({ onAddNotes }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newNotes = {
+      title,
+      description,
+      id: Date.now(),
+      createdAt: new Date().toISOString(),
+    };
+    onAddNotes(newNotes);
+    setTitle("");
+    setDescription("");
+  };
+  return (
+    <div className="add-new-note">
+      <h2>Add New Note</h2>
+      <form className="note-form" onSubmit={handleSubmit}>
+        <input type="text" className="text-field" placeholder="note title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" className="text-field" placeholder="note description" value={description} onChange={(e) => setDescription(e.target.value)} />
+        <button className="btn btn--primary">Add New Note</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddNewNotes;
